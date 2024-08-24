4309374eb3d39f6b487b46d4ca674e4f6b44836e
Jushua <Pabelicjush@gmail.com>
2024-08-25 00:00:24 +0800

    1st Commit

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnNYYtllwo9dfOF636KcGXfNhiBC6EYQI",
+  authDomain: "carmedicdb.firebaseapp.com",
+  databaseURL: "https://carmedicdb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "carmedicdb",
+  storageBucket: "carmedicdb.appspot.com",
+  messagingSenderId: "873364370703",
+  appId: "1:873364370703:web:dc2a463af427cd271d3a7a",
+  measurementId: "G-G6WTDZB2Z9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth with AsyncStorage for persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth };
