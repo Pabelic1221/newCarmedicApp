64c73e1edaff89ad457d3438de007db25385b217
FujiwaraKengo <fujiwara0836@gmail.com>
2024-08-29 19:52:22 +0800

    Fixing Navigation Problem

--- App.js
+++ App.js
@@ -3,14 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './screens/LoginScreen';
-import DrawerNavigator from './DrawerNavigator';
-import UserRegisterScreen from './screens/UserRegisterScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import ShopRegisterScreen from './screens/ShopRegisterScreen'; 
-import RequestScreen from './screens/RequestScreen';
-import ShopListScreen from './screens/ShopListScreen';
-import ReviewsScreen from './screens/ReviewsScreen';
-import FeedbackScreen from './screens/FeedbackScreen';
+import UserRegisterScreen from './screens/UserRegisterScreen';
+import ShopRegisterScreen from './screens/ShopRegisterScreen';
+import DrawerNavigator from './DrawerNavigator'; // Import DrawerNavigator for main app navigation
 
 const Stack = createNativeStackNavigator();
 
@@ -19,18 +15,17 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerShown: false,
+          headerShown: false, // Hide headers for stack screens
         }}
       >
+        {/* Authentication Stack */}
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="UserRegister" component={UserRegisterScreen} />
         <Stack.Screen name="ShopRegister" component={ShopRegisterScreen} />
-        <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} />
-        <Stack.Screen name="RequestService" component={RequestScreen} />
-        <Stack.Screen name="ShopList" component={ShopListScreen} />
-        <Stack.Screen name="Reviews" component={ReviewsScreen} />
-        <Stack.Screen name="Feedback" component={FeedbackScreen} />
+        
+        {/* Main App Navigation */}
+        <Stack.Screen name="Main" component={DrawerNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
